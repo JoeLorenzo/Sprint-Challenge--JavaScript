71ea74e996d0cdd2979f966d6f257d08b5ea7ef9
JoeLorenzo <Joselmedrano@gmail.com>
2019-07-12 19:35:35 -0500

    -Completed overlooked univeristy array section.

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -68,23 +68,21 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 {"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
 
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array.
-
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
-console.log(universities)
+const universities = (graduates.map(graduate => graduate.university))
+const sortedUniversities = universities.sort();
+console.log(sortedUniversities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student.
-
 The resulting contact information should have a space between the first name and the email information like this:
 Name email@example.com
-
 Log the result of your new array. */
-const contactInfo = [];
+const contactInfo = graduates.map(graduate => `Name: ${graduate.first_name} Email: ${graduate.email}`);
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-const uni = [];
+const uni = graduates.filter(graduate => graduate.university.includes('Uni'))
 console.log(uni);
 
 
